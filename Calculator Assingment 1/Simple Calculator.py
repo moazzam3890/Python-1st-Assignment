ae504d53e93aef46f9d3f8d25b447eff1b5b46e3
Moazzam Adil Khan <moaadi3890@gmail.com>
2020-04-26 12:29:05 +0500

    Change in Invalid input print

--- Calculator Assingment 1/Simple Calculator.py
+++ Calculator Assingment 1/Simple Calculator.py
@@ -16,6 +16,6 @@ elif Operation == "/" :
     else :
         print(float(num1) / float(num2))
 else :
-    print("Invalid Input. Please select the correct Input don't be hooshyaar")
+    print("Invalid Input. Please select the correct Input")
 
 print("Thanks for using simple calculator")
